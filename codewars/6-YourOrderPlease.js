57799bbccbd232b3bbfb5b8b3978bb8e7466072c
John Son <jsson77@gmail.com>
2017-01-07 17:22:33 +0100

    editedword through filter

--- codewars/6-YourOrderPlease.js
+++ codewars/6-YourOrderPlease.js
@@ -8,9 +8,28 @@
 
 function order(words){
   // ...
+
+  let dict = {}
+
+  words.split(' ').forEach(word => {
+    let place
+
+    let editedWord = word.split('').filter(letter => {
+      if (Number(letter)) place = letter
+      else return letter
+    }).join('')
+    console.log('editedWord: ', editedWord);
+
+    // dict[place] =
+    console.log('place: ', place);
+  })
+
+  //Find the place
+  //put into dict like 'place':word
+  //iterate through dict to make the sentence.
 }
 
-console.log(order(is2 Thi1s T4est 3a))
+console.log(order('is2 Thi1s T4est 3a'))
 
 // Test.assertEquals(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
 // Test.assertEquals(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
