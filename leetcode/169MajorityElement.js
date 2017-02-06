a952ef4b845ea5b52db64905bd7ca8501192deca
John Son <jsson77@gmail.com>
2017-02-06 12:21:34 +0100

    lc169 done

--- leetcode/169MajorityElement.js
+++ leetcode/169MajorityElement.js
@@ -1,3 +1,33 @@
 // Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 //
 // You may assume that the array is non-empty and the majority element always exist in the array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let map = {}
+
+    nums.forEach(num => {
+        if (map[num]) {
+            map[num] = map[num] + 1
+        } else {
+            map[num] = 1
+        }
+    })
+
+    let keys = Object.keys(map)
+
+    let max = 0
+    let ans
+
+    keys.forEach(key => {
+        if (map[key] > max) {
+            max = map[key]
+            ans = key
+        }
+    })
+
+    return Number(ans)
+};
