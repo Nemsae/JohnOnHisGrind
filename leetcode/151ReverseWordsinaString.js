5a836ce10bd553a3ef74fee05f034b63fa6c79bc
John Son <jsson77@gmail.com>
2017-02-06 11:53:07 +0100

    lc151 done

--- leetcode/151ReverseWordsinaString.js
+++ leetcode/151ReverseWordsinaString.js
@@ -3,3 +3,22 @@
 // For example,
 // Given s = "the sky is blue",
 // return "blue is sky the".
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+    let arr = str.trim().split(' ')
+    let ans = []
+
+    for (let i = arr.length-1; i>=0; i--) {
+        let ele = arr[i]
+        if (ele.length !== 0) {
+            ans.push(ele)
+        }
+
+    }
+
+    return ans.join(' ')
+};
