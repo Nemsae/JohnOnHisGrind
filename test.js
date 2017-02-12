c774ba668c82c9df9c1261148e39f1dbdc4dc4ca
John Son <jsson77@gmail.com>
2017-02-12 15:32:48 +0100

    initialization

--- test.js
+++ test.js
@@ -12,3 +12,14 @@ var totalHammingDistance = function(nums) {
 
     console.log('strs: ', strs)
 };
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var totalHammingDistance = function(nums) {
+    let strs = nums.map(num => (
+        num.toString(2).split('').reverse().join('')
+    ))
+
+    console.log('strs: ', strs)
+};
