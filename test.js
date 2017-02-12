d66c0bb46c634053df186514fc72ccdbd0a474be
John Son <jsson77@gmail.com>
2017-02-12 15:27:36 +0100

    initialization

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 //  Running tests for sleepy
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var totalHammingDistance = function(nums) {
+    let strs = nums.map(num => (
+        num.toString(2).split('').reverse().join('')
+    ))
+
+    console.log('strs: ', strs)
+};
