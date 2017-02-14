df3ac01984654b2eda7037c96077ad10623feefb
John Son <jsson77@gmail.com>
2017-02-14 15:48:46 +0100

    leetcode 344 reverse string

---
+++ leetcode/344ReverseString.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+    return s.split('').map((letter, i, arr) => {
+        return arr[s.length- i-1]
+    }).join('')
+};
