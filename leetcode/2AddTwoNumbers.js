958d3bd3706e458d91b32829418cc6b7b5ea88c7
John Son <jsson77@gmail.com>
2017-02-26 16:02:24 +0100

    lc2 while loop, checking values of each node

--- leetcode/2AddTwoNumbers.js
+++ leetcode/2AddTwoNumbers.js
@@ -17,4 +17,19 @@ var addTwoNumbers = function(l1, l2) {
     let ansList;
     let ansList2;
     let ans = [];
+
+    while (l1 || l2) {
+        let val3 = (l1 && l1.val) + (l2 && l2.val)
+        console.log('count: ', count)
+        console.log(count, 'l1: ', (l1 && l1.val))
+        console.log(count, 'l2: ', (l2 && l2.val))
+        if ((l2 && l2.val) === null) {
+            val3 = l1.val
+            ansList.next = new ListNode(val3)
+            console.log('ALPHA1: ', val3)
+        } else if ((l1 && l1.val) === null) {
+            val3 = l2.val
+            ansList.next = new ListNode(val3)
+            console.log('ALPHA2: ', ansList)
+        }
 }
