19f9e12a507f0cb7f683621f61b502f6e2f9bc94
John Son <jsson77@gmail.com>
2017-02-26 16:01:29 +0100

    lc2 start

---
+++ leetcode/2AddTwoNumbers.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    console.log('l1: ', l1.val, 'l2: ', l2.val)
+    let count = 1;
+    let carry = 0;
+    let ansList;
+    let ansList2;
+    let ans = [];
+}
