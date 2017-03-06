6e8c882b00048636f5a658f04563ffc1ea9a60b1
John Son <jsson77@gmail.com>
2017-03-05 19:05:44 -0800

    lc389FindtheDifference init, optimized, done

--- leetcode/389FindtheDifference.js
+++ leetcode/389FindtheDifference.js
@@ -4,6 +4,23 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
+    let diff = 0
+    console.log(s.charCodeAt(3))
+    console.log(s.charCodeAt(4))
+    for (let i = 0; i < Math.max(s.length, t.length); i++) {
+        diff ^= s.charCodeAt(i)
+        diff ^= t.charCodeAt(i)
+    }
+    return String.fromCharCode(diff)
+};
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+
+var findTheDifference = function(s, t) {
     let sSort = s.split('').sort()
     let tSort = t.split('').sort()
     let sLen = sSort.length
