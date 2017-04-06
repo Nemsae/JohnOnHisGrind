24fb8b183487eeaf0fee4f5fb17aef3281d43993
John Son <jsson77@gmail.com>
2017-04-06 19:09:11 +0200

    code minified, i feel disgusted

--- codewars/6-StopgninnipSMysdroW!.js
+++ codewars/6-StopgninnipSMysdroW!.js
@@ -3,23 +3,7 @@
 // Past Solutions
 // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
-function spinWords(str){
-  //TODO Have fun :)
-  console.log('str: ', str)
-
-  let arr = str.split(' ')
-  let ans = arr.map(word => {
-    // console.log('word: ', word);
-    if (word.length > 4) {
-      let spinned = word.split('').reverse().join('')
-      return spinned
-    } else {
-      return word
-    }
-  }).join(' ')
-
-  return ans
-}
+let spinWords=s=>s.split(' ').map(x=>x.length>4?x.split('').reverse().join(''):x).join(' ')
 
 let string = 'Hey fellow warriors'
 //  Hey wollef sroirraw
