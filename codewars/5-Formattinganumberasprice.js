ba6a26caf722af02a74e23f504f63ad2e82df975
John Son <jsson77@gmail.com>
2017-04-10 11:28:27 +0200

    cw5 refactored

--- codewars/5-Formattinganumberasprice.js
+++ codewars/5-Formattinganumberasprice.js
@@ -11,65 +11,112 @@
 
 var numberToPrice = function(number) {
   let string = number.toString()
-  let negFlag = false
-  let fillerFlag = false
 
-  if (number < 0) negFlag = true
   if (typeof number !== 'number') return 'NaN'
-
-  console.log('string: ', string, 'negFlag is : ', negFlag)
 
   let price = string.split('.')
 
   //  if no period, need to know so we add zeroes
+  let fillerFlag = false
   if (price.length === 1) fillerFlag = true
-
-  console.log('price: ', price,'fillerFlag is: ', fillerFlag)
 
   let firstPart = price[0].split('').reverse()
   let firstArr = []
 
-  console.log('firstPart: ', firstPart)
   for (let i = 0; i < price[0].length; i++) {
     let char = firstPart[i]
     let next = firstPart[i+1]
-//     console.log('char: ', char, 'i: ', i)
-//     if ((i+1) % 3 === 0) {
-//       console.log('Sanity: ', i+1, (i+1) % 3)
-//       firstArr.push(char)
-//       firstArr.push(',')
-//     } else {
-//       firstArr.push(char)
-//     }
+
     if (next === '-' || next === undefined) {
       firstArr.push(char)
     } else {
       !((i+1) % 3) ? firstArr.push(char, ',') : firstArr.push(char)
     }
-
   }
 
-  console.log('firstArr: ', firstArr)
-//   if (firstArr.reverse()[0] === ',') {
-//     firstArr.pop()
-//   }
-
   let firstAns = firstArr.reverse().join('')
-  console.log('firstAns: ', firstAns)
-
 
   let secondAns = ''
 
   if (fillerFlag) {
-    console.log('filler needed!')
     secondAns = '00'
   } else {
     let secondPart = price[1]
     let firstDigit = price[1][0]
     let secondDigit = price[1][1] || '0'
     secondAns = firstDigit + secondDigit
-    console.log('secondPart: ', secondPart, 'secondAns: ', secondAns)
   }
 
   return firstAns + '.' + secondAns
 }
+
+
+// var numberToPrice = function(number) {
+//   let string = number.toString()
+//   let negFlag = false
+//   let fillerFlag = false
+//
+//   if (number < 0) negFlag = true
+//   if (typeof number !== 'number') return 'NaN'
+//
+//   console.log('string: ', string, 'negFlag is : ', negFlag)
+//
+//   let price = string.split('.')
+//
+//   //  if no period, need to know so we add zeroes
+//   if (price.length === 1) fillerFlag = true
+//
+//   console.log('price: ', price,'fillerFlag is: ', fillerFlag)
+//
+//   let firstPart = price[0].split('').reverse()
+//   let firstArr = []
+//
+//   console.log('firstPart: ', firstPart)
+//   for (let i = 0; i < price[0].length; i++) {
+//     let char = firstPart[i]
+//     let next = firstPart[i+1]
+// //     console.log('char: ', char, 'i: ', i)
+// //     if ((i+1) % 3 === 0) {
+// //       console.log('Sanity: ', i+1, (i+1) % 3)
+// //       firstArr.push(char)
+// //       firstArr.push(',')
+// //     } else {
+// //       firstArr.push(char)
+// //     }
+//     if (next === '-' || next === undefined) {
+//       firstArr.push(char)
+//     } else {
+//       !((i+1) % 3) ? firstArr.push(char, ',') : firstArr.push(char)
+//     }
+//
+//   }
+//
+//   console.log('firstArr: ', firstArr)
+// //   if (firstArr.reverse()[0] === ',') {
+// //     firstArr.pop()
+// //   }
+//
+//   let firstAns = firstArr.reverse().join('')
+//   console.log('firstAns: ', firstAns)
+//
+//
+//   let secondAns = ''
+//
+//   if (fillerFlag) {
+//     console.log('filler needed!')
+//     secondAns = '00'
+//   } else {
+//     let secondPart = price[1]
+//     let firstDigit = price[1][0]
+//     let secondDigit = price[1][1] || '0'
+//     secondAns = firstDigit + secondDigit
+//     console.log('secondPart: ', secondPart, 'secondAns: ', secondAns)
+//   }
+//
+//   return firstAns + '.' + secondAns
+// }
+
+// Test.assertEquals(numberToPrice(1500.129),   '1,500.12');
+// Test.assertEquals(numberToPrice(-5),         '-5.00');
+// Test.assertEquals(numberToPrice(1000000.5),  '1,000,000.50');
+// Test.assertEquals(numberToPrice('@'),        'NaN');
