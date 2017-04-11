c78e9ea7a2ec60b25b406840e6209cf603c861b0
John Son <jsson77@gmail.com>
2017-04-11 16:39:51 +0200

    CW6-FindParityOutlier init and done

---
+++ codewars/6-FindTheParityOutlier.js
@@ -0,0 +1,31 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.
+//
+// For example:
+//
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+//
+// Should return: 11
+//
+// [160, 3, 1719, 19, 11, 13, -21]
+//
+// Should return: 160
+
+// function findOutlier(integers){
+//   //your code here
+//   let odds = []
+//   let evens = []
+//
+//   integers.forEach(num => {
+//     num % 2 ? odds.push(num) : evens.push(num)
+//   })
+//
+//   return odds.length === 1 ? odds[0] : evens[0]
+// }
+
+function findOutlier(integers){
+  //your code here
+  let odds = integers.filter(num => num%2)
+  let evens = integers.filter(num => !(num%2))
+
+  return odds.length === 1 ? odds[0] : evens[0]
+}
