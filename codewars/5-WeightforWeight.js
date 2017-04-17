4f6a0a60070a35ad5d8e35c1fce2fc811260a9de
John Son <jsson77@gmail.com>
2017-04-16 20:38:36 -0700

    managed to get custom sort done with string number comparison!

--- codewars/5-WeightforWeight.js
+++ codewars/5-WeightforWeight.js
@@ -8,6 +8,7 @@
 
 function orderWeight(str) {
     console.log('str: ', str);
+    if (str === '') return ''
     // your code
     let dict = {}
     let weights = {}
@@ -28,13 +29,13 @@ function orderWeight(str) {
       console.log('num: ', num);
 
       //  I need to be able to know if a 'weight' is already present,
-      if (weights[s]) {
-        console.log('WEIGHT EXISTS!');
-        weights[s]++
-        wFlag = true
-      } else {
-        weights[s] = 1
-      }
+      // if (weights[s]) {
+      //   console.log('WEIGHT EXISTS!');
+      //   weights[s]++
+      //   wFlag = true
+      // } else {
+      //   weights[s] = 1
+      // }
 
       if (dict[num]) {
         // console.log('KEY EXISTS:', dict);
@@ -60,8 +61,8 @@ function orderWeight(str) {
     }
 
     let keys = Object.keys(dict)
-
-    return keys.sort((a, b) => {
+    console.log('keys: ', keys);
+    let x = keys.sort((a, b) => {
       // if (dict[a] === dict[b]) {
       //   console.log('Same!')
       //   let x = a.split('.')
@@ -84,8 +85,23 @@ function orderWeight(str) {
       //     console.log('Sanity:0000000');
       //   }
       // }
-      return  dict[a] - dict[b]
-    }).join(' ')
+      if (dict[a] === dict[b]) {
+        console.log('SAME WEIGHT DETECTED', 'a: ', a, 'b: ', b)
+        console.log('a-b: ', a-b);
+        // console.log('a>b: ', a>b);
+        // console.log('a<b: ', a<b);
+        if (a < b) return -1
+        else return 1
+        // return a > b
+      } else {
+        return  dict[a] - dict[b]
+      }
+    // }).join(' ')
+  }).map(num2 => Math.floor(num2)).join(' ')
+  // }).join(' ')
+
+    // console.log('x: ', x);
+    return x
 
     // console.log('keys: ', keys);
     //
@@ -94,18 +110,22 @@ function orderWeight(str) {
 let str1 = "103 123 4444 99 2000"
 let str2  = "2000 10003 1234000 44444444 9999 11 11 22 123"
 let str3 = '71899703 200 6 91 425 4 67407 7 96488 6 4 2 7 31064 9 7920 1 34608557 27 72 18 81'
-// console.log(orderWeight(str1))
-// console.log(orderWeight(str2))
-// console.log('String: ', str2);
-console.log("Answer: 11 11 2000 10003 22 123 1234000 44444444 9999")
-console.log(orderWeight(str3))
-console.log('String: ', str3);
-console.log("Answer: 1 2 200 4 4 6 6 7 7 18 27 72 81 9 91 425 31064 7920 67407 96488 34608557 71899703")
+let str4 = '306579 57 496488 2 169924 116 247561 46 346314 200 147405 24 243225 56 373770 178 75149 163 125274 18 92'
+
+// console.log('Answer:', orderWeight(str2))
+// console.log("Answer: 11 11 2000 10003 22 123 1234000 44444444 9999")
+
+// console.log('Answer:', orderWeight(str3))
+// console.log("Answer: 1 2 200 4 4 6 6 7 7 18 27 72 81 9 91 425 31064 7920 67407 96488 34608557 71899703")
+
+console.log('Answer:', orderWeight(str4))
+console.log('Expected: 2 200 24 116 18 163 46 56 92 57 178 243225 125274 147405 346314 247561 75149 373770 306579 169924 496488')
 
 // 71899703 200 6 91 425 4 67407 7 96488 6 4 2 7 31064 9 7920 1 34608557 27 72 18 81
 // Expected: '1 2 200 4 4 6 6 7 7 18 27 72 81 9 91 425 31064 7920 67407 96488 34608557 71899703',
-// instead got: '1 2 200 4 4.1 6 6.1 7 7.1 81 9 72 27 18 91 425 31064 7920 67407 96488 34608557 71899703'
 
+// 306579 57 496488 2 169924 116 247561 46 346314 200 147405 24 243225 56 373770 178 75149 163 125274 18 92
+// Expected: '2 200 24 116 18 163 46 56 92 57 178 243225 125274 147405 346314 247561 75149 373770 306579 169924 496488'
 
 // Test.it("Basic tests",function() {
 //     Test.assertEquals(orderWeight("103 123 4444 99 2000"), "2000 103 123 4444 99")
