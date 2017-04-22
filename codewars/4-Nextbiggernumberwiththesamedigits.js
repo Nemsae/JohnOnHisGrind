a8878b558a261da70a5f1cbb490b44b0ca6e48b6
John Son <jsson77@gmail.com>
2017-04-22 12:01:54 -0700

    init

---
+++ codewars/4-Nextbiggernumberwiththesamedigits.js
@@ -0,0 +1,22 @@
+// You have to create a function that takes a positive integer number and returns the next bigger number formed by the same digits:
+//
+// nextBigger(12)==21
+// nextBigger(513)==531
+// nextBigger(2017)==2071
+// If no bigger number can be composed using those digits, return -1:
+//
+// nextBigger(9)==-1
+// nextBigger(111)==-1
+// nextBigger(531)==-1
+
+function nextBigger(n){
+  //your code here
+
+
+}
+
+// Test.assertEquals(nextBigger(12),21)
+// Test.assertEquals(nextBigger(513),531)
+// Test.assertEquals(nextBigger(2017),2071)
+// Test.assertEquals(nextBigger(414),441)
+// Test.assertEquals(nextBigger(144),414)
