87563844417abcdc72d6f661741ec727a144c61a
John Son <jsson77@gmail.com>
2017-04-22 12:23:24 -0700

    going to splice instead of push and unshift

--- codewars/4-Nextbiggernumberwiththesamedigits.js
+++ codewars/4-Nextbiggernumberwiththesamedigits.js
@@ -14,19 +14,33 @@ function nextBigger(n){
 
   let s = n.toString().split('')
 
-  console.log('s: ', s);
+  // console.log('s0: ', s);
+  let flag = false
+
+  let ans = []
 
   for (let i = s.length - 1; i >= 0; i--) {
     let num = +s[i]
+    let prev = +s[i-1]
+    let tmp = num
+    let tmp2 = prev
 
-    console.log('num: ', num);
+    // console.log('num: ', num);
+    if (num > prev) ans.push(num, prev) , i--
+    else ans.unshift(num)
+    console.log('ans: ', ans);
+    // if (flag) return
   }
+
+  return ans.join('')
+
+  // console.log('s1: ', s);
 
 }
 
-console.log(nextBigger(12)) //  21
-console.log(nextBigger(513)) // 531
-console.log(nextBigger(2017)) //  2071
+// console.log(nextBigger(12)) //  21
+// console.log(nextBigger(513)) // 531
+// console.log(nextBigger(2017)) //  2071
 console.log(nextBigger(414)) // 441
 console.log(nextBigger(144)) // 414
 
