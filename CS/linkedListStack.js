d98e02d035d338e735387b0d6bf82c70c88350f0
John Son <jsson77@gmail.com>
2017-04-24 13:49:25 -0700

    finished with creating simple linkeList Stack class

--- CS/linkedListStack.js
+++ CS/linkedListStack.js
@@ -7,21 +7,68 @@ function LinkedList() {
 
 LinkedList.prototype.push = function (val) {
   let node = {
-    this.val = val
-    this.next = null
+    value: val,
+    next: null
   }
 
   if (this.head === null) {
+    console.log('Sanity:Head is null');
     this.head = node
   } else {
     let current = this.head
+    console.log('current: ', current);
 
-    while (current) {
-      if (current.next === null) {
-        current.next = node
-      }
-
+    while (current.next !== null) {
       current = current.next
     }
+
+    current.next = node
   }
 };
+
+LinkedList.prototype.pop = function () {
+  let current = this.head
+
+  if (current === null) return null
+  if (current.next === null) {
+    let x = current.value
+    this.head = null
+
+    return x
+  }
+
+  while (current.next.next !== null) {
+    current = current.next
+  }
+  let x = current.next.value
+
+  current.next = null
+
+  return x
+};
+
+LinkedList.prototype.peek = function () {
+  let current = this.head
+  if (current === null) return null
+  if (current.next === null) return current.value
+
+  while (current.next !== null) {
+    current = current.next
+  }
+
+  return current.value
+};
+
+let myList = new LinkedList
+// console.log('myList: ', myList);
+
+console.log('1: ', myList.push(1))
+console.log('Peek: ', myList.peek())
+console.log('2: ', myList.push(2))
+console.log('Peek: ', myList.peek())
+console.log('myList:pushed ', myList);
+console.log('3: ', myList.pop())
+console.log('Peek: ', myList.peek())
+console.log('4: ', myList.pop())
+console.log('Peek: ', myList.peek())
+console.log('myList:popped ', myList);
