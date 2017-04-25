0f9e3f59e2cdca1521f7c147adff6de5b43f599e
John Son <jsson77@gmail.com>
2017-04-24 18:39:46 -0700

    LinkedList class style init

---
+++ CS/linkedList.js
@@ -0,0 +1,15 @@
+class LinkedList {
+  constructor(val) {
+    if (val === undefined) {
+      this.head = null
+      this.next = null
+
+    } else {
+      this.head = val
+      this.next = null
+    }
+  }
+
+  
+
+}
