a5d62b35d235ee7974bd5ca62e2a8e9f9847fc19
John Son <jsson77@gmail.com>
2017-04-30 17:34:19 -0700

    completed another java challenge, java string manipulation +5 exp

--- codewarsJava/7-HighestandLowest.java
+++ codewarsJava/7-HighestandLowest.java
@@ -4,34 +4,18 @@ public class Kata {
     int max = Integer.parseInt(strArr[0]);
     int min = Integer.parseInt(strArr[0]);
 
-    System.out.println("numbers: " + numbers);
-    System.out.println("max: " + max);
-    System.out.println("min: " + min);
-
-//     for (String str: strArr) {
-//       System.out.println(str);
-//     }
-
     for (int i = 0; i < strArr.length; i++) {
-/*       String num = strArr[i]; */
       int num = Integer.parseInt(strArr[i]);
 
       if (num > max) {
         max = num;
       }
-      
+
       if (num < min) {
         min = num;
       }
-
-      System.out.println("num: " + num);
     }
 
-    String answer = "";
-    answer += max;
-    answer += " ";
-    answer += min;
-
-    return answer;
+    return String.format("%d %d", max, min);
   }
 }
