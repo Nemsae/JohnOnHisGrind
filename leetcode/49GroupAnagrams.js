f8571a8319756d26f08aa360a3e133723828c2f7
John Son <jsson77@gmail.com>
2017-05-02 19:42:25 -0700

    first algo approach to it done, but TC is too long

--- leetcode/49GroupAnagrams.js
+++ leetcode/49GroupAnagrams.js
@@ -10,11 +10,9 @@
 // ]
 // Note: All inputs will be in lower-case.
 
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
+
 var groupAnagrams = function(strs) {
+  // console.log('strs: ', strs);
   let ans = []
   let dict = {}
 
@@ -22,33 +20,71 @@ var groupAnagrams = function(strs) {
 
   for (let i = 0; i < strs.length; i++) {
     let cur = strs[i]
-    cur.forEach(char => dict[char] ? dict[char]++ : dict[char] = 1)
+    // console.log('cur: ', cur);
+
+    if (cur === '') {
+      dict['empty'] = 1
+    } else {
+      cur.split('').forEach(char => {
+        dict[char] ? dict[char]++ : dict[char] = 1
+      })
+    }
+
     group.push(cur)
+
+    // console.log('group: ', group);
 
     for (let j = i + 1; j < strs.length; j++) {
       let next = strs[j]
+      let skip = false
+      let tmp = JSON.parse(JSON.stringify(dict))
+      // console.log('next: ', next, 'dict: ', dict,  'tmp: ', tmp );
 
-      next.forEach(char => {
-        if (dict[char]) {
-          dict[char]--
-        } else {
-          return;
+      if (next === '') {
+        tmp['empty']--
+        // console.log('dict of empty: ', dict);
+      } else {
+        next.split('').forEach(char => {
+          if (tmp[char]) {
+            tmp[char]--
+          } else {
+            skip = true
+            return;
+          }
+        })
+      }
+
+      if (skip) {
+        // console.log('SKIPPED')
+      } else {
+        let keys = Object.keys(tmp)
+        // console.log('tmp: ', tmp);
+        // console.log('keys: ', keys);
+        let sum = keys.reduce((c, i, a) => {
+          return c + tmp[i]
+        }, 0)
+        // console.log('sum: ', sum);
+
+        if (sum === 0) {
+          strs.splice(j, 1)
+          j--
+
+          group.push(next)
+          // console.log('group: ', group, 'strs: ', strs );
         }
-      })
-
-      let keys = Object.keys(dict)
-      let sum = keys.reduce((c, i, a) => c + i, 0)
-
-      if (sum === 0) {
-        strs.splice(j, 1)
-        strs.length--
-        group.push(next)
       }
     }
 
     ans.push(group)
+    // console.log('ans: ', ans);
+    group = []
     dict = {}
   }
 
   return ans
 };
+
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+// console.log(groupAnagrams(["","b"]))
+// console.log(groupAnagrams(["","b",""]))
+console.log(groupAnagrams(["compilations","bewailed","horology","lactated","blindsided","swoop","foretasted","ware","abuts","stepchild","arriving","magnet","vacating","relegates","scale","melodically","proprietresses","parties","ambiguities","bootblacks","shipbuilders","umping","belittling","lefty","foremost","bifocals","moorish","temblors","edited","hint","serenest","rendezvousing","schoolmate","fertilizers","daiquiri","starr","federate","rectal","case","kielbasas","monogamous","inflectional","zapata","permitted","concessions","easters","communique","angelica","shepherdess","jaundiced","breaks","raspy","harpooned","innocence","craters","cajun","pueblos","housetop","traits","bluejacket","pete","snots","wagging","tangling","cheesecakes","constructing","balanchine","paralyzed","aftereffects","dotingly","definitions","renovations","surfboards","lifework","knacking","apprises","minimalism","skyrocketed","artworks","instrumentals","eardrums","hunching","codification","vainglory","clarendon","peters","weeknight","statistics","ay","aureomycin","lorrie","compassed","speccing","galen","concerto","rocky","derision","exonerate","sultrier","mastoids","repackage","cyclical","gowns","regionalism","supplementary","bierce","darby","memorize","songster","biplane","calibrates","decriminalizes","shack","idleness","confessions","snippy","barometer","earthing","sequence","hastiness","emitted","superintends","stockades","busywork","dvina","aggravated","furbelow","hashish","overextended","foreordain","lie","insurance","recollected","interpreted","congregate","ranks","juts","dampen","gaits","eroticism","neighborhoods","perihelion","simulations","fumigating","balkiest","semite","epicure","heavier","masterpiece","bettering","lizzie","wail","batsmen","unbolt","cudgeling","bungalow","behalves","refurnishes","pram","spoonerisms","cornered","rises","encroachments","gabon","cultivation","parsed","takeovers","stampeded","persia","devotional","doorbells","psalms","cains","copulated","archetypal","cursores","inbred","paradigmatic","thesauri","rose","stopcocks","weakness","ballsier","jagiellon","torches","hover","conservationists","brightening","dotted","rodgers","mandalay","overjoying","supervision","gonads","portage","crap","capers","posy","collateral","funny","garvey","ravenously","arias","kirghiz","elton","gambolled","highboy","kneecaps","southey","etymology","overeager","numbers","ebullience","unseemly","airbrushes","excruciating","gemstones","juiciest","muftis","shadowing","organically","plume","guppy","obscurely","clinker","confederacies","unhurried","monastic","witty","breastbones","ijsselmeer","dublin","linnaeus","dervish","bluefish","selectric","syllable","pogroms","pacesetters","anastasia","pandora","foci","bipartisan","loomed","emits","gracious","warfare","uncouples","augusts","portray","refinery","resonances","expediters","deputations","indubitably","richly","motivational","gringo","hubris","mislay","scad","lambastes","reemerged","wart","zirconium","linus","moussorgsky","swopped","sufferer","sputtered","tamed","merrimack","conglomerate","blaspheme","overcompensate","rheas","pares","ranted","prisoning","rumor","gabbles","lummox","lactated","unzipping","tirelessly","backdate","puzzling","interject","rejections","bust","centered","oxymoron","tangibles","sejong","not","tameness","consumings","prostrated","rowdyism","ardent","macabre","rustics","dodoes","warheads","wraths","bournemouth","staffers","retold","stiflings","petrifaction","larkspurs","crunching","clanks","briefest","clinches","attaching","extinguished","ryder","shiny","antiqued","gags","assessments","simulated","dialed","confesses","livelongs","dimensions","lodgings","cormorants","canaries","spineless","widening","chappaquiddick","blurry","lassa","vilyui","desertions","trinket","teamed","bidets","mods","lessors","impressiveness","subjugated","rumpuses","swamies","annotations","batiks","ratliff","waxwork","grander","junta","chutney","exalted","yawl","joke","vocational","diabetic","bullying","edit","losing","banns","doleful","precision","excreting","foals","smarten","soliciting","disturbance","soggily","gabrielle","margret","faded","pane","jerusalem","bedpan","overtaxed","brigs","honors","repackage","croissants","kirov","crummier","limeades","grandson","criers","bring","jaundicing","omnibusses","gawking","tonsillectomies","deodorizer","nosedove","commence","faulkner","adultery","shakedown","wigwag","wiper","compatible","ultra","adamant","distillation","gestates","semi","inmate","onlookers","grudgingly","recipe","chaise","dialectal","aphids","flimsier","orgasm","sobs","swellheaded","utilize","karenina","irreparably","preteen","mumble","gingersnaps","alumnus","chummiest","snobbish","crawlspaces","inappropriate","ought","continence","hydrogenate","eskimo","desolated","oceanic","evasive","sake","laziest","tramps","joyridden","acclimatized","riffraff","thanklessly","harmonizing","guinevere","demanded","capabler","syphilitics","brainteaser","creamers","upholds","stiflings","walt","luau","deafen","concretely","unhand","animations","map","limbos","tranquil","windbreakers","limoges","varying","declensions","signs","green","snowbelt","homosexual","hopping","residue","ransacked","emeritus","pathologist","brazenly","forbiddingly","alfredo","glummest","deciphered","delusive","repentant","complainants","beets","syntactics","vicissitude","incompetents","concur","canaan","rowdies","streamer","martinets","shapeliness","videodiscs","restfulness","rhea","consumed","pooching","disenfranchisement","impoverishes","behalf","unsuccessfully","complicity","ulcerating","derisive","jephthah","clearing","reputation","kansan","sledgehammer","benchmarks","escutcheon","portfolios","mandolins","marketable","megalomaniacs","kinking","bombarding","wimple","perishes","rukeyser","squatter","coddle","traditionalists","sifts","agglomerations","seasonings","brightness","spices","claimant","sofas","ambulatories","bothered","businessmen","orly","kinetic","contracted","grenadiers","flooding","dissolved","corroboration","mussed","squareness","alabamans","dandelions","labyrinthine","pot","waxwing","residential","pizza","overjoying","whelps","overlaying","elanor","tented","masterminded","balsamed","powerhouses","tramps","eisenstein","voile","repellents","beaus","coordinated","wreckers","eternities","untwists","estrangements","vitreous","embodied"]))
