7920ed897f1d20cf419053ee43e67e54f60fc301
John Son <jsson77@gmail.com>
2017-05-10 19:48:47 -0700

    bubble sort done

--- CS/bubbleSort.js
+++ CS/bubbleSort.js
@@ -1,7 +1,33 @@
-let arr = [9,4,1,2,5,6,10,13,12,3,7,8]
+let arr1 = [9,4,1,2,5,6,10,13,12,3,7,8]
 
-function bubble (arr) {
-  
+function bubble (array) {
+  let lastElement = array.length - 1
+  let isSorted = false
+
+  while (!isSorted) {
+    isSorted = true
+    for (let i = 0; i < lastElement; i++) {
+      if (array[i] > array[i+1]) {
+        swap(array, i, i+1)
+        isSorted = false
+      }
+    }
+    console.log('array: ', array);
+    //  decrement lastElement
+    lastElement--
+  }
+
+
+  function swap (arr, i, j) {
+    let iEle = arr[i]
+    let jEle = arr[j]
+    let temp = iEle
+
+    arr[i] = jEle
+    arr[j] = temp
+  }
+
+  return array
 }
 
-console.log(bubble(arr))
+console.log(bubble(arr1))
