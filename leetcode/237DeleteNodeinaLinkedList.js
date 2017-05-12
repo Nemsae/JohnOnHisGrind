a3fe8aa3f3fabd39c0307f92b107d3daabbc3021
John Son <jsson77@gmail.com>
2017-05-12 13:40:00 -0700

    237DeleteNode recursive solution done

--- leetcode/237DeleteNodeinaLinkedList.js
+++ leetcode/237DeleteNodeinaLinkedList.js
@@ -2,18 +2,34 @@
 //
 // Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} node
- * @return {void} Do not return anything, modify node in-place instead.
- */
-var deleteNode = function(node) {
-    node.val = node.next.val;
-    node.next = node.next.next;
-};
+
+  // * Definition for singly-linked list.
+  // * function ListNode(val) {
+  // *     this.val = val;
+  // *     this.next = null;
+  // * }
+  // */
+ /**
+  * @param {ListNode} node
+  * @return {void} Do not return anything, modify node in-place instead.
+  */
+
+ var deleteNode = function(node) {
+     deleteN(node)
+
+     function deleteN (node) {
+         if (node === null) return null
+
+         let nextVal = deleteN(node.next)
+         console.log('node: ', node, 'nextVal: ', nextVal)
+         if (nextVal === null) {
+             node.next = null
+         }
+
+         let temp = node.val
+         node.val = nextVal
+         if (node.next && node.next.val === null) node.next = null
+         console.log('node: ', node, 'nextVal: ', nextVal, 'temp: ', temp)
+         return temp
+     }
+ };
