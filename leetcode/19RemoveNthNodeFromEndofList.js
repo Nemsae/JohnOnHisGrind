65ba2018fa624a776a725c66c4123341d17e56d8
John Son <jsson77@gmail.com>
2017-05-12 11:51:44 -0700

    lc19 done, clogged

--- leetcode/19RemoveNthNodeFromEndofList.js
+++ leetcode/19RemoveNthNodeFromEndofList.js
@@ -21,40 +21,31 @@ Try to do this in one pass.
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    let ans = recurse(head, null, n)
+ var removeNthFromEnd = function(head, n) {
+     return recurse(head, null, n)
 
-    function recurse(node, prev, n) {
-        console.log(0, 'node: ', node, 'prev: ', prev)
-        if (node === null) return 0
-        let x = recurse(node.next, node, n)
-        let count = typeof x === 'number' ? x + 1 : x
-        console.log('n: ', n, 'count: ', count)
-        if (count === n && prev === null) {
-            console.log(1, 'node: ', node, 'prev: ', prev)
-            // if (prev === null) {
-            //     console.log('sanity')
-            //     return null
-            // }
-            return null
+     function recurse(node, prev, n) {
+         console.log(0, 'node: ', node, 'prev: ', prev)
+         if (node === null) return 0
+         let x = recurse(node.next, node, n)
+         let count = typeof x === 'number' ? x + 1 : x
+         console.log('n: ', n, 'count: ', count)
+         if (count === n && node === head) {
+             console.log(1, 'node: ', node, 'prev: ', prev)
+             // if (prev === null) {
+             //     console.log('sanity')
+             //     return null
+             // }
+             return node.next
 
-        } else if (count === n) {
-            console.log(2, 'node: ', node, 'prev: ', prev)
-            prev.next = node.next
-            console.log(2, 'node: ', node, 'prev: ', prev)
+         } else if (count === n) {
+             console.log(2, 'node: ', node, 'prev: ', prev)
+             prev.next = node.next
+             console.log(2, 'node: ', node, 'prev: ', prev)
 
-            return prev
-        } else {
-            return count
-        }
-    }
-
-    if (ans === null) {
-        return null
-    } else {
-        return ans
-    }
-
-    // return
-
-};
+             return head
+         } else {
+             return count
+         }
+     }
+ };
