255d01dd65c7913bd031655e714944b6ffcb25c2
John Son <jsson77@gmail.com>
2017-05-13 12:27:16 -0700

    lc2AddTwoNumbers revisted

--- leetcode/2AddTwoNumbers.js
+++ leetcode/2AddTwoNumbers.js
@@ -1,10 +1,73 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+
+
+//  May 13, 2017
+var addTwoNumbers = function(one, two) {
+    let carry = 0,
+        l1 = one,
+        l2 = two,
+        ansHead,
+        l1Prev,
+        l2Prev
+
+    while (l1 !== null || l2 !== null) {
+        let sum = 0
+        //  console.log('one: ', one, 'two: ', two)
+
+        if (l1 === null) {
+            //  console.log('Snt:two ', two)
+            ansHead = two
+        } else if (l2 === null) {
+            //  console.log('Snt:one ', one)
+            ansHead = one
+        }
+
+        l1 && (sum += l1.val)
+        l2 && (sum += l2.val)
+
+        if (carry) sum++
+        if (sum > 9) {
+         carry = 1
+         sum = sum - 10
+        } else {
+         carry = 0
+        }
+
+        l1 && (l1.val = sum)
+        l2 && (l2.val = sum)
+
+        l1 && (l1Prev = l1)
+        l2 && (l2Prev = l2)
+
+        l1 && (l1 = l1.next)
+        l2 && (l2 = l2.next)
+    }
+
+    if (carry) {
+        //  console.log('carry at the end')
+        if (ansHead === two) {
+            //  console.log('two is ansHead', two)
+            one.val = 1
+            one.next = null
+            l2Prev.next = one
+            //  console.log('two is ansHead', two)
+        } else if (ansHead === one) {
+            //  console.log('one is ansHead', one)
+            two.val = 1
+            two.next = null
+            l1Prev.next = two
+            //  console.log('one is ansHead', one)
+        } else {
+            two.val = 1
+            two.next = null
+            l1Prev.next = two
+        }
+    }
+
+    return ansHead === undefined ? one : ansHead
+};
+
+
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
