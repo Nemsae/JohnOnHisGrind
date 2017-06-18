96d04bfee413afe190cf4a34ece1ff89e2cae99b
John Son <jsson77@gmail.com>
2017-06-17 23:02:02 -0700

    5.1WriteBitsMintoN init

---
+++ algos/CTCI/5.1WriteBitsIntoN.js
@@ -0,0 +1,8 @@
+// Input: two 32-bit numbers m and n, i and j
+// Output: n
+
+// Write a method to insert M into N such that M starts at bit j and ends at bit i.
+
+function writeMintoN(m, n, i, j) {
+
+}
