552cffb67560d167b5e4a06378a5e283e0c20592
John Son <jsson77@gmail.com>
2017-07-07 23:36:16 -0700

    completed calculating payouts

--- eos.js
+++ eos.js
@@ -1,7 +1,6 @@
-
-
 function calculateBenchmark(exchangeRate, myEther) {
   const myTokens = myEther/exchangeRate;
+  console.log('myTokens:benchmark ', myTokens);
   const ethBenchmark = (2000000*myEther)/myTokens;
 
   return ethBenchmark;
@@ -18,19 +17,17 @@ function calculatePayout(exchangeRate, totalEth, myEther) {
 /*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 */
 /*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 */
 /*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 *//*  Window #7 */
-//  CONSTANTS
 const exchangeRate_07_07_17 = .010500;
 const myEther_07_07_17 = 6.74947130;
 const actualTotalEth_07_07_17 = 19948.92;
 
 //  Calculating the limit for total eth contributed
-const estimatedTotalEth_07_07_17 = calculateBenchmark(exchangeRate_07_07_17, myEther_07_07_17);
-console.log('estimatedTotalEth_07_07_17: ', estimatedTotalEth_07_07_17);
+const maxLimitEth_07_07_17 = calculateBenchmark(exchangeRate_07_07_17, myEther_07_07_17);
+console.log('maxLimitEth_07_07_17: ', maxLimitEth_07_07_17);
 
-//  potential payout
-const potentialPayout_07_07_17 = calculatePayout(exchangeRate_07_07_17, estimatedTotalEth_07_07_17, myEther_07_07_17);
-console.log('potentialPayout_07_07_17: ', potentialPayout_07_07_17);
-
-//  actual payout
+//  Actual payout
 const actualPayout_07_07_17 = calculatePayout(exchangeRate_07_07_17, actualTotalEth_07_07_17, myEther_07_07_17);
 console.log('actualPayout_07_07_17: ', actualPayout_07_07_17);
+
+const ethProfit_07_07_17 = actualPayout_07_07_17 - myEther_07_07_17;
+console.log('ethProfit_07_07_17: ', ethProfit_07_07_17);
